a0ff13c3f0c673d3a9c7b2d6314ec6019a40926e
jwa <jonas.wanner@clavisit.com>
2020-10-13 10:42:51 +0200

    add catch if no keywords are defined, add settings button to popup

--- popup/popup.js
+++ popup/popup.js
@@ -1,4 +1,5 @@
-let enabled = true
+let enabled
+let keywords
 
 init()
 
@@ -8,6 +9,7 @@ function init () {
 
 function set (storage) {
   enabled = storage.enabled || false
+  keywords = storage.keywords || false
 
   if (enabled) {
     document.body.classList.remove('disabled')
@@ -16,13 +18,26 @@ function set (storage) {
     document.body.classList.remove('enabled')
     document.body.classList.add('disabled')
   }
+
+  if (!keywords) {
+    document.body.classList.add('no-keywords')
+    browser.storage.local.set({ enabled: false }).then(init)
+  }
 }
 
 for (const button of document.querySelectorAll('.button')) {
   button.addEventListener('click', () => {
+    if (!keywords) {
+      return
+    }
+
     if (!enabled) {
       browser.search.search({ query: '' })
     }
     browser.storage.local.set({ enabled: !enabled }).then(init)
   })
 }
+
+document.querySelector('.settings').addEventListener('click', () => {
+  browser.runtime.openOptionsPage()
+})
