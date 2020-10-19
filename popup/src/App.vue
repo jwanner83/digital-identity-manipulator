b5840c4d64a33d5ef6d311ac6e7ff0a293f28238
jwa <jonas.wanner@clavisit.com>
2020-10-19 14:40:17 +0200

    first changes for the new popup inspired by the beautiful and stunning Firefox Relay Add-on

---
+++ popup/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <Header />
+  <Status />
+</template>
+
+<script>
+import Header from '@/components/Header/index'
+import Status from '@/components/Status/index'
+
+export default {
+  name: 'App',
+  components: {
+    Status,
+    Header
+  }
+}
+</script>
+
+<style lang="scss">
+html, body {
+  width: 300px;
+  margin: 0;
+  border: 1px solid;
+}
+
+#app {
+  font-family: system-ui, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
+}
+</style>
---
+++ popup/src/components/Status/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="status">
+    <p class="status-text">currently inactive</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'status'
+}
+</script>
+
+<style lang="scss">
+.status {
+  text-align: center;
+  border-top: 1px #ccc solid;
+  border-bottom: 1px #ccc solid;
+
+  &-text {
+    font-size: 14px;
+    color: #999;
+    font-style: italic;
+  }
+}
+</style>
